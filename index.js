3346ca859bfb25cbdcbb7d982daa4a84681add9d
deboker <117391017+deboker@users.noreply.github.com>
2023-10-20 10:04:45 +0200

    k

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function handleClick() {
         .then(data => {
             remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
-            console.log(deckId)
+            
         })
 }
 
